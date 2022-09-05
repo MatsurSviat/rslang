bd483ee0b1832f5f3155725faad949b9e76ef6c1
Kairat <dzhumabaev.kai@gmail.com>
2022-09-06 04:00:11 +0600

    deploy

--- index.ts
+++
@@ -1,4 +0,0 @@
-import { start } from "./src/main";
-import "./src/styles/style.css";
-
-start();
--- src/assets/audios/audios.ts
+++
@@ -1,5 +0,0 @@
-function AudioPlay(audiofile: string): string {
-  return `assets/audios/${audiofile}.mp3`;
-}
-
-export default AudioPlay;
